022c838bc8a15401e416c7a33f70584de908ab43
yali325 <153582948+yali325@users.noreply.github.com>
2026-05-15 20:02:20 +0800

    集成调度框架Quartz

--- batch/src/main/java/com/yali/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/yali/batch/job/SpringBootTestJob.java
@@ -1,14 +1,10 @@
 package com.yali.batch.job;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableScheduling
+//@Component
+//@EnableScheduling
 public class SpringBootTestJob {
 
-    @Scheduled(cron = "0/5 * * * * ?")
+//    @Scheduled(cron = "0/5 * * * * ?")
     public void test(){
         System.out.println(".....yali.....");
     }
---
+++ batch/src/main/java/com/yali/batch/job/TestQuartzJob.java
@@ -0,0 +1,4 @@
+package com.yali.batch.job;
+
+public class TestQuartzJob {
+}
